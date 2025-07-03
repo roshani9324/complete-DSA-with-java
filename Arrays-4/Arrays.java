8950f3c9f810811b5983fa798405b36ff6f6d9dd
roshanimaurya.in@gmail.com <avasheshmaurya419@gmail.com>
2025-07-04 01:54:41 +0530

    complete Arrays

---
+++ Arrays-4/Arrays.java
@@ -0,0 +1,14 @@
+
+public class Arrays {
+    public static void main(String[] args) {
+        // Example usage of the Arrays class
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("Array length: " + numbers.length);
+        
+        // Print elements of the array
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+    
+}
